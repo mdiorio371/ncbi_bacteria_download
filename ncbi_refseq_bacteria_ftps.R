cf5990f18116503ce86b27c1164923f15c5d6a15
Matt D'Iorio <36821142+mdiorio371@users.noreply.github.com>
2020-09-28 15:52:51 -0400

    Add files via upload

---
+++ ncbi_refseq_bacteria_ftps.R
@@ -0,0 +1,86 @@
+#!/usr/bin/env Rscript --vanilla
+
+list.of.packages <-
+  c("magrittr", "dplyr", "data.table")
+new.packages <- 
+  list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)>0) install.packages(new.packages)
+
+suppressMessages(require(magrittr))
+suppressMessages(require(dplyr))
+suppressMessages(require(data.table))
+
+#arguments
+args <- commandArgs(trailingOnly=TRUE)
+database <- as.character(args[1])
+outdir <- as.character(args[2])
+
+## Access the refseq genomes
+refseq <- 
+  fread(
+    "https://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/assembly_summary.txt", 
+    stringsAsFactors = F, quote = "", select = c(1,5,11,12,16,18,19,20)
+  ) %>% 
+  filter(
+    assembly_level =="Complete Genome" & 
+      version_status =="latest"
+  ) %>%
+  `colnames<-`(c("assembly", colnames(.)[-1])) %>%
+  as_tibble
+## Access the Genbank genomes that are not in refseq
+genbank <- 
+  fread(
+    "https://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt", 
+    stringsAsFactors = F, quote = "", select = c(1,5,11,12,16,18,19,20)
+  ) %>% 
+  filter(
+    assembly_level =="Complete Genome" & 
+      version_status =="latest"
+  ) %>%
+  `colnames<-`(c("assembly", colnames(.)[-1])) %>%
+  as_tibble %>%
+  filter(paired_asm_comp != "identical")
+
+## Combine the rows and extract the ftp links
+if (database=="all"){
+  all_ftp_paths <- 
+    bind_rows(refseq, genbank) %>%
+    pull(ftp_path) %>%
+    sprintf(
+      "%s/%s_genomic.fna.gz",
+      ., gsub(".*/", "", .)
+    )
+  ## write out all of the ftp paths to one file
+  fileConn <- 
+    file(sprintf("%s/all_seqs.txt",outdir))
+  writeLines(
+    ftp_paths, 
+    fileConn
+  )
+  close(fileConn)
+} else if (database=="representative") {
+  # just the representative paths
+  representative_ftp_paths <- 
+    bind_rows(refseq, genbank) %>%
+    filter(refseq_category=="representative genome") %>%
+    pull(ftp_path) %>%
+    sprintf(
+      "%s/%s_genomic.fna.gz",
+      ., gsub(".*/", "", .)
+    )
+  
+  
+  ## write out only the representative of the ftp paths to one file
+  fileConn <- 
+    file(sprintf("%s/representative_seqs.txt",outdir))
+  writeLines(
+    ftp_paths, 
+    fileConn
+  )
+  close(fileConn)
+} else {
+  stop("First argument must be either 'all' or 'representative'")
+}
+
+
+
